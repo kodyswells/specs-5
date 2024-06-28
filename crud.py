670d41c247948d48c44e6e392718775464c86c4b
Kody Wells <kodyswells@gmail.com>
2024-06-28 13:27:01 -0600

    finshed part 3

--- crud.py
+++ crud.py
@@ -18,6 +18,18 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_movie():
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    return Movie.query.get(movie_id)
+
+def get_user():
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
